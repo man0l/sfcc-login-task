60bd049397f8f3559df76cfedaa1f45f776cf1b5
Manol Trendafilov <manol.trendafilov@gmail.com>
2021-11-27 09:28:31 +0200

    extend home controller

--- cartridges/home_show_extender/cartridge/controllers/Home.js
+++ cartridges/home_show_extender/cartridge/controllers/Home.js
@@ -2,3 +2,12 @@
 
 var server = require('server');
 server.extend(module.superModule);
+
+server.append('Show', function (req, res, next) {
+    var viewData = res.getViewData();
+    viewData.customjson = { test: 1 };
+    res.setViewData(viewData);
+    next();
+});
+
+module.exports = server.exports();
