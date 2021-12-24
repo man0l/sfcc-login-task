f8866c49271ecf8d6e4fa3861e49b5697058533b
Manol Trendafilov <manol.trendafilov@gmail.com>
2021-12-24 17:13:56 +0200

    add newsletter subscription controller

--- cartridges/app_custom_newsletter/cartridge/controllers/Subscribe.js
+++ cartridges/app_custom_newsletter/cartridge/controllers/Subscribe.js
@@ -1,2 +1,83 @@
 'use strict';
 
+var server = require('server');
+var UUIDUtils = require('dw/util/UUIDUtils');
+var Transaction = require('dw/system/Transaction');
+var CustomObjectMgr = require('dw/object/CustomObjectMgr');
+
+server.get(
+    'Create',
+    server.middleware.https,
+    function (req, res, next) {
+      var form = req.form;
+      var error = false;
+
+      if (!form) {
+        error = true;
+      }
+
+      var type = 'NewsletterSubscription';
+      var keyValue = UUIDUtils.createUUID();
+
+      try {
+        Transaction.wrap(function() {
+          var newsletter = CustomObjectMgr.createCustomObject(type, keyValue);
+          newsletter.custom.name = form.name;
+          newsletter.custom.email = form.email;
+        });
+      } catch (error) {
+        error = true;
+      }
+
+      if (error) {
+        res.json({
+          error: true
+        });
+      } else {
+        res.json({
+          error: false,
+          id: keyValue
+        });
+      }
+
+      next();
+    }
+);
+
+server.get(
+    'Delete',
+    function (req, res, next) {
+      var form = req.form;
+      var error = false;
+      var type = 'NewsletterSubscription';
+      var keyValue = form.id;
+      if (!form) {
+        error = true;
+      }
+
+      try {
+        var newsletter = CustomObjectMgr.getCustomObject(type, keyValue);
+        Transaction.wrap(function() {
+            CustomObjectMgr.remove(newsletter);
+        });
+       
+      } catch(error) {
+          error = true;
+      }
+
+      if (error) {
+        res.json({
+          error: true
+        });
+      } else {
+        res.json({
+          error: false,
+          id: keyValue
+        });
+      }
+      
+      next();
+    }
+);
+
+module.exports = server.exports();
