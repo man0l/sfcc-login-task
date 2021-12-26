b1ca8bfc197a6f7af79344fb9b343bcbdceb4c7d
Manol Trendafilov <manol.trendafilov@gmail.com>
2021-12-26 13:56:03 +0200

    extend product details page

---
+++ cartridges/plugin_backinstock/cartridge/controllers/Product.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @namespace Product
+ */
+
+ var server = require('server');
+ 
+
+ var cache = require('*/cartridge/scripts/middleware/cache');
+ var consentTracking = require('*/cartridge/scripts/middleware/consentTracking');
+ var pageMetaData = require('*/cartridge/scripts/middleware/pageMetaData');
+ 
+ server.extend(module.superModule);
+ server.append('Show', function(req, res, next) {
+    
+    next();
+ });
+
+ module.exports = server.exports();
