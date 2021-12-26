31858ed63d4faa1523c7bebcdd037204918fd027
Manol Trendafilov <manol.trendafilov@gmail.com>
2021-12-26 11:01:02 +0200

    add new cartridge

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ var path = require('path');
         name: 'js',
         entry: sgmfScripts.createJsPath(),
         output: {
-            path: path.resolve('./cartridges/app_custom_newsletter/cartridge/static'),
+            path: path.resolve('./cartridges/plugin_backinstock/cartridge/static'),
             filename: '[name].js'
         }
     }, {
@@ -15,7 +15,7 @@ var path = require('path');
         name: 'scss',
         entry: sgmfScripts.createScssPath(),
         output: {
-            path: path.resolve('./cartridges/app_custom_newsletter/cartridge/static'),
+            path: path.resolve('./cartridges/plugin_backinstock/cartridge/static'),
             filename: '[name].css'
         },
         module: {
